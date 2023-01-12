7420b869aed9a840ccec4c593dc68bf19c7346e1
Ahammed nihal pt <ahammednihalpt@gmail.com>
2023-01-12 18:30:37 +0530

    frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import Signup from "./Pages/Signup";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Login from "./Pages/Login";
+import Home from "./Pages/Home";
+import AdminHome from "./Pages/AdminHome";
+import AdminUsersPage from "./Pages/AdminUsersPage";
+import AdminAddUserPage from "./Pages/AdminAddUserPage";
+import AdminEditUserPage from "./Pages/AdminEditUserPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<Login />} />
+          <Route path="/signup" exact element={<Signup />} />
+          <Route path="/home" exact element={<Home />} />
+          <Route path="/admin/home" exact element={<AdminHome />} />
+          <Route path="/admin/users" exact element={<AdminUsersPage />} />
+          <Route path="/admin/users/add" exact element={<AdminAddUserPage />} />
+          <Route path="/admin/users/edit" exact element={<AdminEditUserPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
